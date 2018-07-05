9836d6a0f3c827b1ce6a84987790e99f7a12399d
Kiss Péter <kiss.peti86@gmail.com>
2018-07-05 18:01:55 +0200

    LocalDB

---
+++ MVCEventCalendar/db/srcipts.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Events] (
+    [EventId]     INT            IDENTITY (1, 1) NOT NULL,
+    [Subject]     NVARCHAR (255) NOT NULL,
+    [Description] NVARCHAR (300) NULL,
+    [Start]       DATETIME       NOT NULL,
+    [End]         DATETIME       NULL,
+    [ThemeColor]  NVARCHAR (10)  NULL,
+    [IsFullDay]   BIT            NOT NULL,
+    PRIMARY KEY CLUSTERED ([EventId] ASC)
+);
